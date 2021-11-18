ac36b39f46908121abd1fafcbf06c9cdf42c040d
Merrick919 <37953774+Merrick919@users.noreply.github.com>
2021-11-19 02:52:53 +0800

    Update server.js

--- node.js/server.js
+++ node.js/server.js
@@ -1,5 +1,2 @@
 fs=require('fs');
-
-function f(path,res){fs.exists(path,callback=>{if(callback){fs.readFile(path,(err,data)=>{res.end(data)})}else{fs.readFile('public/404.txt',(err,data)=>{res.end(data)})}})}
-
-require('http').createServer(function t(req,res){f('public'+(!require('path').basename(req.url)?'/home.txt':req.url),res)}).listen(80);
+require('http').createServer(function t(req,res){path='public'+(!require('path').basename(req.url)?'/home.txt':req.url);fs.exists(path,callback=>{fs.readFile(callback?(path):'public/404.txt',(err,data)=>{res.end(data)})})}).listen(80);
